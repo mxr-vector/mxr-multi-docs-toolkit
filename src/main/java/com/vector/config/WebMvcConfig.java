4c96538cccb49adb8744ca95aeefcb823e16cfb1
YuanJie <782353676@qq.com>
2025-08-11 20:14:05 +0800

    refactor(config): 调整 WebMvcConfig 包位置并添加导入
    - 将 WebMvcConfig 类从 com.vector.utils.context 包移动到 com.vector.config 包- 添加对 com.vector.utils.context.RequestThreadInterceptor 的导入

--- src/main/java/com/vector/config/WebMvcConfig.java
+++ src/main/java/com/vector/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
-package com.vector.utils.context;
+package com.vector.config;
 
+import com.vector.utils.context.RequestThreadInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
